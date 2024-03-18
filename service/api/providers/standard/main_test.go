f2cd67062f8b3e6820920c0704fe33647291748b
Yair Levi <yairlevi2001@gmail.com>
2024-03-18 17:24:41 +0200

    Fix: test database should have the same setup as regular.

--- service/api/providers/standard/main_test.go
+++ service/api/providers/standard/main_test.go
@@ -2,9 +2,11 @@ package standard
 
 import (
 	"auth/service/database"
+	"auth/service/database/types"
 	"github.com/labstack/echo/v4"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"log"
 	"testing"
 )
@@ -15,11 +17,14 @@ var (
 
 func TestMain(m *testing.M) {
 	log.Println("Before testing")
-	testDb, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	testDb, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
 		panic(err)
 	}
 	database.PrepareDB(testDb)
+	testDb.Create(&types.SecurityConfig{})
 
 	db = testDb
 	testServer = echo.New()
